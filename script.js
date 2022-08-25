be30efce4180a1fc86a5990cc0ac1c8e5bead32f
ADMINASHU <kashutosh02@gmail.com>
2022-08-26 01:52:13 +0530

    First game build

---
+++ script.js
@@ -0,0 +1,71 @@
+const hole = document.getElementById('hole');
+const game = document.getElementById('game');
+const result = document.getElementById('result');
+const text = document.getElementById('text');
+const highScore = document.getElementById('highScore');
+
+let score = 0;
+// let hScore = 0;
+let jumping = 0;
+
+
+
+hole.addEventListener("animationiteration", randomHole)
+
+function randomHole() {
+    const random = -((Math.random()*56)+24);
+    hole.style.top = random+"vh"; 
+    score++;
+}
+
+
+
+
+const fall = setInterval(() => {
+    const birdBottom = parseInt(window.getComputedStyle(bird).getPropertyValue("bottom"))
+    const birdLeft = parseInt(window.getComputedStyle(bird).getPropertyValue("left"))
+    if(jumping == 0 && birdBottom > 0){
+        bird.style.bottom = (birdBottom-4)+"px";
+    }
+    const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"))
+    const holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"))
+    const hTop = -0.95*(holeTop);
+    
+    
+    const gameTop = parseInt(window.getComputedStyle(game).getPropertyValue("top"))
+    const hBottom = hTop - ((gameTop*7.5)*0.25);
+    
+   
+    if ((birdBottom < 0)||(blockLeft < birdLeft*1.298)&&(blockLeft > birdLeft)&&((birdBottom < hBottom)||(birdBottom > hTop))){
+        result.style.display = "grid";
+        text.innerText = `Your final score is : ${score}`;
+        highScore.innerText = `High Score: ${score}`;
+        game.style.display = "none";
+        // score = 0;
+    }             
+  
+},10);
+
+window.addEventListener('keydown',hop)
+
+function hop(){
+    jumping = 1;
+    const birdBottom = parseInt(window.getComputedStyle(bird).getPropertyValue("bottom"));
+    if(birdBottom > 0){
+        bird.style.bottom = (birdBottom + 35) + "px";
+    }
+    setTimeout(()=>{
+        jumping = 0
+    },100 )
+
+}
+
+
+
+
+
+
+function restart(){
+    location.reload();
+    score = 0;
+}
